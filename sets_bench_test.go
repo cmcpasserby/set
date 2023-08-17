2612f7b835926ae382ca8b7356e32e68822f42a0
Chris Cunningham <chris.cunningham@gogiigames.com>
2023-08-17 10:18:46 -0300

    groups benchmarks

--- sets_bench_test.go
+++ sets_bench_test.go
@@ -14,52 +14,56 @@ func testSeqSet(start, count int, b *testing.B) Set[int] {
 	return New(items...)
 }
 
-func BenchmarkSet_Contains(b *testing.B) {
-	set := testSeqSet(0, b.N, b)
-	b.ResetTimer()
+func Benchmark_Contains(b *testing.B) {
+	b.Run("Set", func(b *testing.B) {
+		set := testSeqSet(0, b.N, b)
+		b.ResetTimer()
 
-	for i := 0; i < b.N; i++ {
-		_ = set.Contains(i)
-	}
-}
-
-func BenchmarkMap_Contains(b *testing.B) {
-	set := make(map[int]struct{}, b.N)
-	for i := 0; i < b.N; i++ {
-		set[i] = struct{}{}
-	}
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		_, _ = set[i]
-	}
-}
-
-func BenchmarkSet_Equals(b *testing.B) {
-	lhs := testSeqSet(0, size, b)
-	rhs := testSeqSet(0, size, b)
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		_ = lhs.Equals(rhs)
-	}
-}
-
-func BenchmarkMap_Equals(b *testing.B) {
-	lhs := make(map[int]struct{}, size)
-	rhs := make(map[int]struct{}, size)
-
-	for i := 0; i < size; i++ {
-		lhs[i] = struct{}{}
-		rhs[i] = struct{}{}
-	}
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		for key := range lhs {
-			_, _ = rhs[key]
+		for i := 0; i < b.N; i++ {
+			_ = set.Contains(i)
 		}
-	}
+	})
+
+	b.Run("Map", func(b *testing.B) {
+		set := make(map[int]struct{}, b.N)
+		for i := 0; i < b.N; i++ {
+			set[i] = struct{}{}
+		}
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = set[i]
+		}
+	})
+}
+
+func Benchmark_Equals(b *testing.B) {
+	b.Run("Set", func(b *testing.B) {
+		lhs := testSeqSet(0, size, b)
+		rhs := testSeqSet(0, size, b)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_ = lhs.Equals(rhs)
+		}
+	})
+
+	b.Run("Map", func(b *testing.B) {
+		lhs := make(map[int]struct{}, size)
+		rhs := make(map[int]struct{}, size)
+
+		for i := 0; i < size; i++ {
+			lhs[i] = struct{}{}
+			rhs[i] = struct{}{}
+		}
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			for key := range lhs {
+				_, _ = rhs[key]
+			}
+		}
+	})
 }
 
 func BenchmarkUnion(b *testing.B) {
